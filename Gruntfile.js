892a805292501ae37209e30010e8a9e98426dedc
ericdahl <eric.c.dahl@gmail.com>
2014-07-04 20:28:23 -0500

    enable development mode with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,8 @@
 module.exports = function (grunt) {
     'use strict';
 
+    var sourceFiles = ['www/css/**', 'www/*.html'];
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
@@ -15,12 +17,37 @@ module.exports = function (grunt) {
                 platforms: ['android'],
                 plugins: ['https://github.com/apache/cordova-plugin-device-orientation']
             }
+        },
+        watch: {
+            source: {
+                files: ['<%= jshint.files %>', sourceFiles],
+                tasks: ['default']
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    base: 'dist'
+                }
+            }
+        },
+        copy: {
+            dist: {
+                files: [
+                    { cwd: 'www', expand: true, src: '**',  dest: 'dist/' }
+                ]
+            }
         }
+
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-phonegap');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jshint', 'copy:dist']);
     grunt.registerTask('run', ['default', 'phonegap:build', 'phonegap:run']);
+    grunt.registerTask('dev', ['default', 'connect:server', 'watch:source']);
 };
